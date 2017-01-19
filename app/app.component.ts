9cba89a05ba65496f83db7d807140780a6f9bdb3
Szupera Zita <szuperaz@gmail.com>
2017-01-19 17:46:55 +0100

    angular2 first glance code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+        <h1>Hello {{name}}</h1>
+        <button class="btn btn-default" (click)="counter = counter + 1;">Click</button>
+        <div>The button has been clicked {{counter}} times</div>
+        <datepicker [(ngModel)]="date" [showWeeks]="showWeeks"></datepicker>
+    `,
+    styles: [
+        `
+        datepicker {
+            display: inline-block;
+        }
+    `]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+    name = 'Angular';
+    counter = 0;
+    date = new Date();
+    showWeeks = true;
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, Ng2BootstrapModule.forRoot()],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
